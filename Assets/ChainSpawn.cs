0487c06fcc6dea6b0f862841a2b7955ae7f2a4f9
Robert Christensen <robert.christensen@mymail.champlain.edu>
2020-02-15 15:52:56 -0500

    Added projectile support to slashes

---
+++ Assets/ChainSpawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChainSpawn : MonoBehaviour
+{
+    public GameObject prototype;
+
+    void Start()
+    {
+        GameObject o = Instantiate(prototype, transform.position, transform.rotation, transform.parent.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
